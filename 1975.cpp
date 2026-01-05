70ad701e967a84f00df28c7419b97b54b667a678
OmendraNaharwal <omendraji03@gmail.com>
2026-01-05 22:36:12 +0530

    Add implementation for maxMatrixSum function

---
+++ 1975.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    long long maxMatrixSum(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        long long sum =0;
+        int cnt=0;
+        int mina = INT_MAX;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                sum+=abs(matrix[i][j]);
+                if(matrix[i][j]<0) cnt++;
+                mina = min(mina,abs(matrix[i][j]));
+            }
+        }
+        if(cnt%2==0) return sum;
+        else return sum - 2*mina;
+    }
+};
