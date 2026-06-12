b9c387b6f06e8349ef8bafc1e325b71ad76918b3
Omendra Naharwal <omendraji03@gmail.com>
2026-06-12 22:01:48 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (5.96%) - LeetSync

---
+++ 886-score-of-parentheses/score-of-parentheses.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int scoreOfParentheses(string s) {
+        stack<int> st;
+        st.push(0);
+        for(char ch:s){
+            if(ch=='('){
+                st.push(0);
+            }else{
+                int val = st.top();
+                st.pop();
+                int score = max(2*val,1);
+                int top = st.top();
+                st.pop();
+                st.push(top+score);
+            }
+        }
+        return st.top();
+    }
+};
